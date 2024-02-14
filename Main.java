3ed5b5d7f8536fe38bbeac513dede2d3d97b2315
Oszkár Leopold <oszkar.leopold@smartymind.hu>
2024-02-14 19:10:13 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    private final static String PATH = "C:\\Felhasználók";
+
+    private final static String NON_EXISTING_PATH = "C\\something";
+    private final static String CODER = "OSCAR";
+
+    public static void main(String[] args) {
+
+        System.out.println("Existing path: " + CODER + " path " + PATH);
+        System.out.println("Non Existing path: " + NON_EXISTING_PATH);
+
+    }
+}
